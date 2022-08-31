ce7ffc892b6c27dfa3b8471a9ed67ddde01ea31a
Felipe Kawassaki <fekawassaki@gmail.com>
2022-08-31 13:19:53 +0100

    Creating the Palette of colors

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,38 +2,20 @@ import { ThemeProvider } from "styled-components";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Button } from "../components/Button";
-import { defaultTheme } from "../styles/themes";
+import light from "../styles/themes/light";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
+  title: "Components/Button",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => (
-  <ThemeProvider theme={defaultTheme}>
-    <Button {...args} />
+  <ThemeProvider theme={light}>
+    <Button {...args}>Button</Button>
   </ThemeProvider>
 );
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+export const Simple = Template.bind({});
+Simple.args = {
   variant: "primary",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  variant: "secondary",
-};
-
-export const Neutral = Template.bind({});
-Neutral.args = {
-  variant: "neutral",
-};
-
-export const Warning = Template.bind({});
-Warning.args = {
-  variant: "warning",
 };
---
+++ src/stories/Palette/index.styled.ts
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  
+  max-width: 1680px;
+  max-height: 100vh;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+export const ColorSpan = styled.span`
+  ${({ color, textColor }: { color: string, textColor: string }) => css`
+    display: block;
+    padding: 2rem;
+    background-color: ${color};
+    color: ${textColor};
+    font-family: 'Roboto', sans-serif;
+    width: 13rem;
+  `}
+`;
+
+export const ColorPaletteSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+`
